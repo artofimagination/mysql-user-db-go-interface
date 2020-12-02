67c0511540e4533f69d004fb3ed8620e018593e0
Art of Science and Imagination <72178744+artofimagination@users.noreply.github.com>
2020-12-03 11:38:40 +1300

    Update httpConnector.py

--- test/httpConnector.py
+++ test/httpConnector.py
@@ -3,7 +3,7 @@ import time
 
 class HTTPConnector():
   def __init__(self):
-    self.URL = "http://localhost:8080"
+    self.URL = "http://127.0.0.1:8080"
     connected = False
     timeout = 15
     while timeout > 0:
