928e40a7dd0ffe99e3f04d6ab8aa37997074a0c7
Art of Science and Imagination <artofimag@gmail.com>
2021-09-24 12:03:10 +0200

    Issue#53: fix CI issue

--- curlTest.sh
+++ curlTest.sh
@@ -1,1 +1,2 @@
-curl --header "Content-Type: application/json"   --request POST   --data '{"email": "testEmailOwnerGetProject2", "password": "dGVzdFBhc3N3b3Jk", "username": "testUserOwnerGetProject2"}'   http://localhost:8181/add-user
+curl --header "Content-Type: application/json"   --request POST   --data '{"email": "testEmailOwnerGetProject2", "password": "dGVzdFBhc3N3b3Jk", "username": "testUserOwnerGetProject2"}'   http://0.0.0.0:8181/add-user
+curl --header "Content-Type: application/json"   --request POST   --data '{"password": "dGVzdFBhc3N3b3Jk", "username": "testUserOwnerGetProject2"}'   http://0.0.0.0:8181/add-user
