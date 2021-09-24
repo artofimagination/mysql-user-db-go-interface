bf3ee9ae167056f2a49131b897020d3ccd9aeceb
Art of Science and Imagination <artofimag@gmail.com>
2021-09-24 12:11:03 +0200

    Issue#53: fix CI issue

--- curlTest.sh
+++ curlTest.sh
@@ -1,3 +1,4 @@
 curl --header "Content-Type: application/json"   --request POST   --data '{"email": "testEmailOwnerGetProject2", "password": "dGVzdFBhc3N3b3Jk", "username": "testUserOwnerGetProject2"}'   http://0.0.0.0:8181/add-user
 curl --header "Content-Type: application/json"   --request POST   --data '{"password": "dGVzdFBhc3N3b3Jk", "username": "testUserOwnerGetProject2"}'   http://0.0.0.0:8181/add-user
+pip3 install -r tests/requirements.txt && pytest -v tests
 docker ps && docker logs user-db-server
